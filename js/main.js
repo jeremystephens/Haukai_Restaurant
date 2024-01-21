51db93aedf9fdaf609a57bd12ad66bb358bcd1a3
Jeremy Stephens <65746951+jeremystephens@users.noreply.github.com>
2024-01-21 21:58:06 +1300

    Update main.js
    Added style to map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,217 @@
 // Function to initialize the Google Map
 function initMap() {
-    var location = { lat: -35.2081675, lng: 173.9367333};
+
+    var location = { lat: -35.21799936266406, lng: 173.96239149713313 };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
-        center: location
+        center: location,
+        disableDefaultUI: true,
+        styles: [
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#686868"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#f2f2f2"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "lightness": "-22"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "saturation": "11"
+                    },
+                    {
+                        "lightness": "-51"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "saturation": "3"
+                    },
+                    {
+                        "lightness": "-56"
+                    },
+                    {
+                        "weight": "2.20"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "lightness": "-52"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "weight": "6.13"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "lightness": "-10"
+                    },
+                    {
+                        "gamma": "0.94"
+                    },
+                    {
+                        "weight": "1.24"
+                    },
+                    {
+                        "saturation": "-100"
+                    },
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": "-16"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "saturation": "-41"
+                    },
+                    {
+                        "lightness": "-41"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "weight": "5.46"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "weight": "0.72"
+                    },
+                    {
+                        "lightness": "-16"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "lightness": "-37"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#b7e4f4"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            }
+        ]
     });
     var marker = new google.maps.Marker({
         position: location,
@@ -17,7 +225,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector('.header');
 
     window.addEventListener("scroll", function() {
-        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+        let currentScroll = window.scrollY || document.documentElement.scrollTop;
         
         if (currentScroll > lastScrollTop) {
             header.style.top = "-100px";
